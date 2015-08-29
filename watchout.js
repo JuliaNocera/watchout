591548caa777c81a3795ec7bf26b64ca3b0fc114
Julia Nocera <giulz42@gmail.com>
2015-08-28 19:48:07 -0700

    add a player, animate player movement with mouse

--- watchout.js
+++ watchout.js
@@ -11,7 +11,27 @@ var createEnemies = function() {
   })
 }
 
-var enemies = board.selectAll("circle").data(createEnemies, function(item) {return item.id}).enter().append("svg:circle");
+
+var player = board.append("circle").attr("class", ".player");
+player.attr("cx", window.innerWidth / 2)
+player.attr("cy", window.innerHeight / 2)
+player.attr("points", "05,30 15,10 25,30")
+player.attr("r", 20);
+player.attr("fill", "red");
+//console.log(d3.event.dx)
+function dragFunc(event) {
+  player.attr("cx", d3.event.dx + parseInt(player.attr("cx")));
+  player.attr("cy", d3.event.dy + parseInt(player.attr("cy")));
+}
+
+var drag = d3.behavior.drag().on('drag', dragFunc)
+
+player.call(drag);
+
+
+
+
+var enemies = board.selectAll("circle").filter(".enemy").data(createEnemies, function(item) {return item.id}).enter().append("svg:circle").attr("class", ".enemy");
 enemies.style({"r": 20, "fill": "blue"});
 
 var update = function(){
