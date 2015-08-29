029a1e06a2b7fec20e618683d033a8bf4bbf2ae0
Julia Nocera <giulz42@gmail.com>
2015-08-28 17:10:11 -0700

    enemies added, board setup

--- watchout.js
+++ watchout.js
@@ -1,1 +1,24 @@
 // start slingin' some d3 here.
+var board = d3.select("body").append("svg").style({'height' : window.innerHeight, 'width' : window.innerWidth});
+
+var createEnemies = function() {
+  return _.range(0, 50).map(function(item) {
+    return {
+      id: item,
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+    }
+  })
+}
+
+
+var enemies = board.selectAll("circle").data(createEnemies, function(item) {return item.id}).enter().append("svg:circle");
+enemies.style({"r": 20, "fill": "blue"});
+enemies.attr("cx", function(enemy) {
+  return enemy.x;
+});
+enemies.attr("cy", function(enemy) {
+  return enemy.y;
+});
+
+console.log(enemies);
