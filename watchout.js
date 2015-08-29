6fa9f96ce65a1b488a7f10c70e8e7194204d1cdf
Julia Nocera <giulz42@gmail.com>
2015-08-28 20:29:06 -0700

    detects when enemy collides

--- watchout.js
+++ watchout.js
@@ -2,7 +2,7 @@
 var board = d3.select("body").append("svg").style({'height' : window.innerHeight, 'width' : window.innerWidth});
 
 var createEnemies = function() {
-  return _.range(0, 50).map(function(item) {
+  return _.range(0, 5).map(function(item) {
     return {
       id: item,
       x: Math.random() * window.innerWidth,
@@ -28,8 +28,54 @@ var drag = d3.behavior.drag().on('drag', dragFunc)
 
 player.call(drag);
 
+function checkCol(enemy, collidedCallback) {
+  var radiusSum = 20 + 20
+  var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr("cx"))
+  var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr("cy"))
+  var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) )
+  if(separation < radiusSum) {
+    collidedCallback(player, enemy);
+  }
+
+}
 
 
+var tweenWithCollisionDetection = function(endData) {
+  //Select current passed in eneamy
+  var enemy = d3.select(this);
+
+  var startPos = {
+    x: parseFloat(enemy.attr("cx")),
+    y: parseFloat(enemy.attr("cy")),
+  }
+  var endPos = {
+    x: endData.x,
+    y: endData.y,
+  }
+
+  return function(t) {
+    checkCol(enemy, function() {
+      console.log("t")
+      //Chech if high score is less the current score
+        //Set high score to current
+      //Reset current score
+      //collisions++
+
+    });
+
+    enemyNexPos = {
+      x: startPos.x + (endPos.x - startPos.x)*t,
+      y: startPos.y + (endPos.y - startPos.y)*t,
+    }
+    if(isNaN(enemyNexPos.x) || isNaN(enemyNexPos.y)) {
+      return;
+    }
+
+    return enemy.attr("cx", enemyNexPos.x).attr("cy", enemyNexPos.y);
+
+
+  }
+}
 
 var enemies = board.selectAll("circle").filter(".enemy").data(createEnemies, function(item) {return item.id}).enter().append("svg:circle").attr("class", ".enemy");
 enemies.style({"r": 20, "fill": "blue"});
@@ -43,7 +89,7 @@ var update = function(){
     return enemy.x;
   }).attr("cy", function(enemy) {
     return enemy.y;
-  }).duration(1000).each("end",update);
+  }).duration(1000).tween("custom", tweenWithCollisionDetection).each("end",update);
 };
 
 update();  
